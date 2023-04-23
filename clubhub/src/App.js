c7b9ba82eb59cd3a70811047afe6ecdc889dd4d9
Connor Armstrong <cparmstrong5@berkeley.edu>
2023-04-23 12:15:43 -0700

    working on window useState hooks

--- clubhub/src/App.js
+++ clubhub/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Club from "./Club";
+import ScrollElement from './ScrollElement';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Club id="11" title="ski clubs" body="a fun club where people get together and do blow" url="https.www.gogle.com"/>
+        <ScrollElement title = "scrollTest"></ScrollElement>
         <a
           className="App-link"
           href="https://reactjs.org"
